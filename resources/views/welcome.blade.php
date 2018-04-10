6b7e418ed02a22175ac80d4bd26c8b70374d468d
grand <grand@DESKTOP-A28RCDK>
2018-04-10 16:38:22 +0200

    Message d'accueil si le site est online ("Bonjour")

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Bonjour Anaëlle
+                    Bonjour
                 </div>
   
             </div>
